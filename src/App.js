6adeffc31ee2896a6da6b072c7e5293d63c2061b
nathanWynn <nww15119@uga.edu>
2018-06-21 15:22:08 -0400

    displays a map from google maps api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GoogleMapReact from 'google-map-react';
+
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 class App extends Component {
+
+  static defaultProps = {
+    center: {
+      lat: 33.948059,
+      lng: -83.377304
+    },
+    zoom: 13
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      // Important! Always set the container height explicitly
+     <div style={{ height: '100vh', width: '100%' }}>
+     <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyD_AMMReRmbmb1mpMkc1jX1RWNG6xqu9Ao" }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+         <AnyReactComponent
+           lat={33.948059}
+           lng={-83.377304}
+           text={'Yo!'}
+         />
+       </GoogleMapReact>
+     </div>
     );
   }
 }
