e04f47f1dea9c6fcbbccb4596f20fb05aeef840b
Will Miller <willmiller@Wills-MBP.attlocal.net>
2018-07-09 16:45:44 -0400

    Clean Up of the file tree for completion

--- src/pages/mapPage/mapPage.js
+++ src/pages/mapPage/mapPage.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import Navbar from '../../components/navbar/navbar';
 
-import MyGreatPlace from './my_great_place.jsx';
-import MyGreatPlace2 from './my_great_place2.jsx';
-import MyGreatPlace3 from './my_great_place3';
+
 
 class MapPage extends Component {
     static defaultProps = {
@@ -25,10 +23,6 @@ class MapPage extends Component {
                     defaultCenter={this.props.center}
                     defaultZoom={this.props.zoom}
                 >
-                <MyGreatPlace lat={33.9603887} lng={-83.3739979} text={"A"} />
-                <MyGreatPlace2 lat={33.93369521} lng={-83.4409503} text={"A"} />
-                {/* need to find the lat and lng of one more startup in athens */}
-                <MyGreatPlace3 lat={33.958772} lng={-83.380841} text={"A"} />
                     {/* <AnyReactComponent
                         lat={33.948059}
                         lng={-83.377304}
